b14e7fc41877fc01ff487b858866dc86948adf03
yanglbme <szuyanglb@outlook.com>
2021-10-31 11:20:10 +0800

    feat: update imclient

--- src/main/java/io/github/doocs/im/IMClient.java
+++ src/main/java/io/github/doocs/im/IMClient.java
@@ -12,11 +12,17 @@ import java.util.concurrent.ThreadLocalRandom;
  * @since 2021/07/29 16:11
  */
 public class IMClient {
+    private final Long sdkAppId;
+    private final String userId;
+    private final String key;
+    private String userSig;
+    private long userSigExpireTs;
+
+    private static final String VERSION = "v4";
+    private static final long EXPIRE_TIME = 24 * 60 * 60L;
     private static final ConcurrentHashMap<String, IMClient> IM_CLIENT = new ConcurrentHashMap<>();
     private static final String FORMAT_URL = "https://console.tim.qq.com/%s/%s/%s?sdkappid=%d&identifier=%s&usersig=%s&random=%d&contenttype=json";
-    /**
-     * core methods
-     */
+
     public final Account account;
     public final Message message;
     public final Member member;
@@ -25,17 +31,6 @@ public class IMClient {
     public final SNS sns;
     public final Operation operation;
     public final RecentContact recentContact;
-    /**
-     * init property
-     */
-    private String userSig;
-    private long userSigExpireTs;
-    private final Long sdkAppId;
-    private final String userId;
-    private final String key;
-
-    private static final String VERSION = "v4";
-    private static final long EXPIRE_TIME = 24 * 60 * 60L;
 
     public static IMClient getInstance(Long sdkAppId, String userId, String key) {
         String identify = sdkAppId + "_" + userId;
@@ -51,6 +46,7 @@ public class IMClient {
         this.key = key;
         this.userSig = SigUtil.genUserSig(sdkAppId, key, userId, EXPIRE_TIME);
         this.userSigExpireTs = System.currentTimeMillis() / 1000 + EXPIRE_TIME - 100;
+
         account = new Account(this);
         message = new Message(this);
         member = new Member(this);
@@ -80,13 +76,5 @@ public class IMClient {
             }
         }
         return userSig;
-    }
-
-    @Override
-    public String toString() {
-        return "IMClient{" +
-                "sdkAppId=" + sdkAppId +
-                ", userId='" + userId + '\'' +
-                '}';
     }
 }
