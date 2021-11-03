414f632ecf48286d53766ab3004987ce5d498fcb
yanglbme <szuyanglb@outlook.com>
2021-11-03 16:18:33 +0800

    feat: enable page site

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    lang: 'zh-CN',
+    title: 'Tencent Cloud IM Server SDK Document - Java',
+    description: '腾讯云 IM 服务端 SDK API 文档 Java 版',
+    base: '/qcloud-im-server-sdk-java/',
+    themeConfig: {
+      repo: 'doocs/qcloud-im-server-sdk-java',
+      logo: 'https://avatars.githubusercontent.com/u/43716716?s=200&v=4',
+    },
+  }
---
+++ docs/README.md
@@ -0,0 +1,1 @@
+# Tencent Cloud IM Server SDK API
