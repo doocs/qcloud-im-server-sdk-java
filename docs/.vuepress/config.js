ecf328fa72ec9e5275ffd47ec1cb787bc66d9816
yanglbme <szuyanglb@outlook.com>
2021-11-04 15:04:26 +0800

    docs: add guide document

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,27 +1,66 @@
 module.exports = {
-    lang: 'zh-CN',
-    title: 'Tencent Cloud IM Server SDK Document - Java',
-    description: '腾讯云 IM 服务端 SDK API 文档 Java 版',
-    base: '/qcloud-im-server-sdk-java/',
-    themeConfig: {
-      repo: 'doocs/qcloud-im-server-sdk-java',
-      logo: 'https://avatars.githubusercontent.com/u/43716716?s=200&v=4',
-      open: true,
-      sidebar: [
-        {
-          text: '指南',
-          link: '/guide/',
-          children: [
-            {
-              text: '账户',
-              link: '/guide/account.md',
-            },
-            {
-              text: '消息',
-              link: '/guide/getting-started.md',
-            },
-          ],
-        },
-      ],
-    },
-  }
+  lang: "zh-CN",
+  title: "腾讯云 IM 服务端 Java SDK 使用文档",
+  description: "腾讯云 IM 服务端 SDK API 文档 Java 版",
+  base: "/qcloud-im-server-sdk-java/",
+  themeConfig: {
+    repo: "doocs/qcloud-im-server-sdk-java",
+    logo: "https://avatars.githubusercontent.com/u/43716716?s=200&v=4",
+    open: true,
+    contributorsText: '贡献者',
+    editLinkText: '编辑',
+    lastUpdatedText: '最近更新时间',
+    sidebar: [
+      {
+        text: "指南",
+        link: "/guide/",
+        children: [
+          {
+            text: "介绍",
+            link: "/guide/intro.md",
+          },
+          {
+            text: "快速上手",
+            link: "/guide/quickstart.md",
+          },
+          {
+            text: "帐号管理",
+            link: "/guide/account.md",
+          },
+          {
+            text: "单聊消息",
+            link: "/guide/message.md",
+          },
+          {
+            text: "全员推送",
+            link: "/guide/member.md",
+          },
+          {
+            text: "资料管理",
+            link: "/guide/profile.md",
+          },
+          {
+            text: "关系链管理",
+            link: "/guide/sns.md",
+          },
+          {
+            text: "最近联系人",
+            link: "/guide/recent_contact.md",
+          },
+          {
+            text: "群组管理",
+            link: "/guide/group.md",
+          },
+          {
+            text: "全局禁言管理",
+            link: "/guide/operation_1.md",
+          },
+          {
+            text: "运营管理",
+            link: "/guide/operation_2.md",
+          },
+        ],
+      },
+    ],
+  },
+};
--- docs/.vuepress/configs/index.js
+++
@@ -1,1 +0,0 @@
-export * as sidebar from './sidebar'
