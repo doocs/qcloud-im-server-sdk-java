2a94d478a0ecbcf9712d63ff4d66b43af7cefb2b
Yang Libin <szuyanglb@outlook.com>
2021-11-09 16:50:36 +0800

    feat: add constants (#61)

---
+++ src/main/java/io/github/doocs/im/constant/Condition.java
@@ -0,0 +1,33 @@
+package io.github.doocs.im.constant;
+
+/**
+ * 条件
+ *
+ * @author bingo
+ * @since 2021/11/9 16:40
+ */
+public class Condition {
+    /**
+     * 属性的或条件
+     */
+    public static final String ATTRS_OR = "AttrsOr";
+
+    /**
+     * 属性的与条件
+     */
+    public static final String ATTRS_AND = "AttrsAnd";
+
+    /**
+     * 标签的或条件
+     */
+    public static final String TAGS_OR = "TagsOr";
+
+    /**
+     * 标签的与条件
+     */
+    public static final String TAGS_AND = "TagsAnd";
+
+    private Condition() {
+
+    }
+}
