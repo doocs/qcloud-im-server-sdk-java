1693491a2926ca9974db24147450974b6214c06a
yanglbme <szuyanglb@outlook.com>
2022-06-01 14:31:28 +0800

    fix

--- src/main/java/io/github/doocs/im/ImClient.java
+++ src/main/java/io/github/doocs/im/ImClient.java
@@ -2,14 +2,7 @@ package io.github.doocs.im;
 
 import io.github.doocs.im.constant.ContentType;
 import io.github.doocs.im.constant.Domain;
-import io.github.doocs.im.core.Account;
-import io.github.doocs.im.core.Group;
-import io.github.doocs.im.core.Member;
-import io.github.doocs.im.core.Message;
-import io.github.doocs.im.core.Operation;
-import io.github.doocs.im.core.Profile;
-import io.github.doocs.im.core.RecentContact;
-import io.github.doocs.im.core.Sns;
+import io.github.doocs.im.core.*;
 import io.github.doocs.im.util.SigUtil;
 
 import java.util.concurrent.ThreadLocalRandom;
