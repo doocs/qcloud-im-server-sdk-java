8172dd0d2a0bfe87edc361e659e9b8ca7646142a
yanglbme <szuyanglb@outlook.com>
2022-06-25 12:03:47 +0800

    build: release v0.3.19
    * Add msgDropReason in SendGroupMsgResult
    * Upgrade dependency version
    * Update java doc

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,15 @@ module.exports = {
   lang: "zh-CN",
   title: "腾讯云 IM 服务端 Java SDK 使用文档",
   description: "腾讯云 IM 服务端 SDK API 文档 Java 版",
-  head: [['link', { rel: 'icon', href: 'https://avatars.githubusercontent.com/u/43716716?s=200&v=4' }]],
+  head: [
+    [
+      "link",
+      {
+        rel: "icon",
+        href: "https://avatars.githubusercontent.com/u/43716716?s=200&v=4",
+      },
+    ],
+  ],
   base: "/qcloud-im-server-sdk-java/",
   themeConfig: {
     repo: "doocs/qcloud-im-server-sdk-java",
@@ -13,10 +21,10 @@ module.exports = {
     editLinkText: "编辑",
     lastUpdatedText: "最近更新时间",
     locales: {
-      '/': {
-        tip: '说明',
-        warning: '注意',
-        danger: '警告',
+      "/": {
+        tip: "说明",
+        warning: "注意",
+        danger: "警告",
       },
     },
     sidebar: [
