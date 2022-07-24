e6b31173fe5b64ec348e37d53d5b8b88ed67262c
yanglbme <szuyanglb@outlook.com>
2022-07-24 21:16:15 +0800

    fix

--- src/main/java/io/github/doocs/im/model/group/MemberProfile.java
+++ src/main/java/io/github/doocs/im/model/group/MemberProfile.java
@@ -176,6 +176,9 @@ public class MemberProfile {
         private String memberAccount;
         private String role;
         private Integer joinTime;
+        /**
+         * 消息序列号，用于标记该条消息，数据范围 [0,4294967295]
+         */
         private Long msgSeq;
         private String msgFlag;
         private Integer lastSendMsgTime;
