f0f7fc9ccca396512f44b86f2271b6161253851e
yanglbme <szuyanglb@outlook.com>
2023-01-10 17:08:45 +0800

    docs: add code comment

--- src/main/java/io/github/doocs/im/model/response/ImportGroupTopicResult.java
+++ src/main/java/io/github/doocs/im/model/response/ImportGroupTopicResult.java
@@ -9,6 +9,9 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  * @since 2023/1/10 16:29
  */
 public class ImportGroupTopicResult extends GenericResult {
+    /**
+     * 创建成功之后的话题 ID，由 IM 云后台分配或者用户指定
+     */
     @JsonProperty("TopicId")
     private String topicId;
 
