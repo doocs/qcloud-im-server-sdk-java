8a5b055275b4d23dfd65b2fd1fb368ac3a80f5a0
acbin <44314231+acbin@users.noreply.github.com>
2023-10-09 10:47:53 +0000

    chore: upgrade dependencies version

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
-const { defaultTheme } = require('vuepress');
-const { searchPlugin } = require('@vuepress/plugin-search');
+import { defaultTheme } from 'vuepress';
+import { searchPlugin } from '@vuepress/plugin-search';
 
 module.exports = {
   lang: 'zh-CN',
