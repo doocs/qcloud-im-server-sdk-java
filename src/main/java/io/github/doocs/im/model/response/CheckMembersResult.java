a9307f4e1cf433b3907c5fc05f6a214c0010959c
yanglbme <szuyanglb@outlook.com>
2024-01-09 20:26:11 +0800

    feat: add new group api
    * 设置/取消直播群管理员
    * 获取直播群管理员列表
    * 查询用户是否在直播群内

---
+++ src/main/java/io/github/doocs/im/model/response/CheckMembersResult.java
@@ -0,0 +1,37 @@
+package io.github.doocs.im.model.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * 查询用户是否在直播群内-结果
+ *
+ * @author bingo
+ * @since 2024/1/9 11:36
+ */
+public class CheckMembersResult extends GenericResult implements Serializable {
+    private static final long serialVersionUID = 983416554639985218L;
+
+    @JsonProperty("Member_Account")
+    private List<String> memberAccount;
+
+    public List<String> getMemberAccount() {
+        return memberAccount;
+    }
+
+    public void setMemberAccount(List<String> memberAccount) {
+        this.memberAccount = memberAccount;
+    }
+
+    @Override
+    public String toString() {
+        return "CheckMembersResult{" +
+                "memberAccount=" + memberAccount +
+                ", actionStatus='" + actionStatus + '\'' +
+                ", errorInfo='" + errorInfo + '\'' +
+                ", errorCode=" + errorCode +
+                '}';
+    }
+}
